8e55d517a399e79231b17b2948ba21d07f37129d
Milena Gouveia <milenaagouveia@gmail.com>
2022-05-16 19:15:07 +0100

    Created 'Activity' class

---
+++ src/lib/model/entities/activity.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+
+/// Stores information about an extra activity.
+///
+/// The information stored is:
+/// - Activity 'id'
+/// - The 'name' of the activity
+/// - The 'description' of the activity
+/// - The 'startingDate' and 'endingDate' of the activity
+/// - The activity 'frequency'
+/// - The activity 'color label'
+
+enum Frequency {
+  noRepetition,
+  everyDay,
+  everyWeek,
+  everyMonth,
+  everyYear,
+}
+
+class Activity {
+  final int id;
+  String name;
+  String description;
+  DateTime startingDate;
+  DateTime endingDate;
+  Frequency frequency;
+  Color colorLabel;
+
+  Activity(
+      {this.id,
+      @required String this.name,
+      String this.description = '',
+      @required DateTime this.startingDate,
+      @required DateTime this.endingDate,
+      Frequency this.frequency = Frequency.noRepetition,
+      Color this.colorLabel = null});
+
+  Map<String, dynamic> toMap(restaurantId) {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'description': this.description,
+      'startingDate': this.startingDate,
+      'endingDate': this.endingDate,
+      'frequency': this.frequency,
+      'colorLabel':this.colorLabel};
+  }
+}
