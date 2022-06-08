973345d42b9f5018496ab8fc61e5cc5bbc719686
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-06-08 21:01:13 +0100

    Changed goToCalendarPAge step definition to when

--- src/test_driver/steps/go_to_calendar_page.dart
+++ src/test_driver/steps/go_to_calendar_page.dart
@@ -3,11 +3,11 @@ import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 
 StepDefinitionGeneric goToCalendarPage() {
-  return given1<String, FlutterWorld>(
+  return when1<String, FlutterWorld>(
       'I go to the {string} page', (key, context) async {
       final navigationDrawerLocator = find.byValueKey('navigation-drawer');
-      final pageLocator = find.byValueKey(key);
       await FlutterDriverUtils.tap(context.world.driver, navigationDrawerLocator);
+      final pageLocator = find.byValueKey(key);
       await FlutterDriverUtils.tap(context.world.driver, pageLocator);
     }
   );
